8d734d551efc5204e3afbffd6052068ab1850180
sergiopazmino <sergioedwin.2003@gmail.com>
2025-01-28 10:43:57 -0500

    Rutas actualizadas

--- routes/web.php
+++ routes/web.php
@@ -83,3 +83,6 @@ Route::group(['middleware' => ['auth']], function () {
 Route::group(['middleware' => ['auth']], function () {});
 
 Route::get('/activity_log', [Activity_logController::class, 'index'])->middleware(Authenticate::class . ':admin');
+
+Route::post('/add-repuestos', [ProductoController::class, 'addRepuestos'])->name('add-repuestos');
+
